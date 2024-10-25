2ed402d2f6ee5506ee174b4fbabbf7cf79ea87e8
Emanuel Rita <emanuelrita@gmail.com>
2024-10-25 15:08:55 +0100

    new inpu

--- app/pages/testvalues/componentValue.tsx
+++ app/pages/testvalues/componentValue.tsx
@@ -1,7 +1,7 @@
 export default function ComponentValue({ StringValue }: { StringValue: string }) {
   return (
-    <div>
+    
       <p className="font-bold">{StringValue}</p>
-    </div>
+      
   );
 }
--- app/pages/testvalues/page.tsx
+++ app/pages/testvalues/page.tsx
@@ -3,29 +3,48 @@ import { useState } from "react";
 import { useRef } from "react";
 import ComponentValue from "./componentValue";
 
-export default function ValuesPage(){
-
-const [counter, setCount] = useState(0);
-const var1 = "hello";
 
 
-const counter2 = useRef(1);
 
-const handleClick = () => {
-    setCount(counter + 1);
-}
+export default function ValuesPage() {
 
+    const [estado, setEstado] = useState(0);
+    const var1 = "hello";
+
+
+    const [counter, setCount] = useState(0);
+    const handleClick = () => {
+        setCount(counter + 1);
+    }
+
+    const counter2 = useRef(1);
+
+    const [inputValue, setInputValue] = useState("INITIAL VALUE");
 
     return (
         <div>
             <h1>{var1}</h1>
-            <h2>Counter: {counter}</h2>
-            <button  className="bg-blue-500 text-white font-bold m-2 p-4 rounded"  onClick={handleClick}>Increment</button>
-            <h3>Value of var2: {counter2.current}</h3>
-            <button className="bg-blue-500 text-white font-bold m-2 p-4 rounded"  onClick={() => counter2.current += 1}>Update var2</button>
             <div>
-                <ComponentValue StringValue="Titulo"/>
+                <h2>Counter: {counter}</h2>
+                <button className="bg-blue-500 text-white font-bold m-2 p-4 rounded" onClick={handleClick}>Increment</button>
             </div>
+            <div>
+                <h3>Value of var2: {counter2.current}</h3>
+                <button className="bg-blue-500 text-white font-bold m-2 p-4 rounded" onClick={() => counter2.current += 1}>Update var2</button>
+            </div>
+            <div>
+                <h2>Estado: {estado}</h2>
+                <button className="bg-blue-500 text-white font-bold m-2 p-4 rounded" onClick={() => setEstado(estado + 1)}>Increment</button>
+            </div>
+            <div className="grid grid-cols-3 gap-4 w-1/2">
+                <div>
+                    <input name="MyInput" className="border text-black w-full" value={inputValue} onChange={e => setInputValue(e.target.value)}></input>
+                </div>
+                <div>
+                    <ComponentValue StringValue={inputValue} />
+                </div>                
+            </div>
+
         </div>
     )
 }
