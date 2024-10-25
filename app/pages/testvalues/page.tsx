7c2dc7f5e0a986487901c31d4181daee52c40054
Emanuel Rita <emanuelrita@gmail.com>
2024-10-25 09:54:17 +0100

    new test values

--- app/pages/testvalues/page.tsx
+++ app/pages/testvalues/page.tsx
@@ -1,17 +1,20 @@
 'use client'
 import { useState } from "react";
 import { useRef } from "react";
+import ComponentValue from "./componentValue";
 
 export default function ValuesPage(){
 
 const [counter, setCount] = useState(0);
 const var1 = "hello";
 
+
 let counter2 = useRef(1);
 
 const handleClick = () => {
     setCount(counter + 1);
 }
+
 
     return (
         <div>
@@ -20,6 +23,9 @@ const handleClick = () => {
             <button  className="bg-blue-500 text-white font-bold m-2 p-4 rounded"  onClick={handleClick}>Increment</button>
             <h3>Value of var2: {counter2.current}</h3>
             <button className="bg-blue-500 text-white font-bold m-2 p-4 rounded"  onClick={() => counter2.current += 1}>Update var2</button>
+            <div>
+                <ComponentValue StringValue="Titulo"/>
+            </div>
         </div>
     )
 }
