53296a83781cc05bce726f74356b781dbe4a16e3
emanuelrita <emanuelrita@gmail.com>
2024-11-11 12:20:28 +0000

    Type Product Declared

--- app/pages/productlist/page.tsx
+++ app/pages/productlist/page.tsx
@@ -1,5 +1,13 @@
 import { createClient } from '@/utils/supabase/server'
 
+type Product = {
+  id: number
+  Name: string
+  Description: string
+  Price: number
+  ProductType: { TypeName: string }[]
+}
+
 export default async function Page() {
   const supabase = await createClient()
   const { data: products } = await supabase
@@ -9,14 +17,14 @@ export default async function Page() {
   return (
     <ul role="list" className="divide-y divide-gray-100">
       {products && products.length > 0 ? (
-        products.map((product) => (
+        products.map((product: Product) => (
           <li key={product.id} className="flex justify-between gap-x-6 py-5">
             <div className="flex min-w-0 gap-x-4">
               <div className="min-w-0 flex-auto">
                 <p className="text-sm font-semibold leading-6 text-gray-900">{product.Name}</p>
                 <p className="mt-1 truncate text-xs leading-5 text-gray-500">{product.Description}</p>
-                {product.ProductType && (
-                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{product.ProductType.TypeName}</p>
+                {product.ProductType && product.ProductType.length > 0 && (
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{product.ProductType[0].TypeName}</p>
                 )}
               </div>
             </div>
