3d4ab2b2bf3a960cfcc1c7ef92441d2ef1cbdc3e
emanuelrita <emanuelrita@gmail.com>
2024-11-11 12:15:43 +0000

    Fix PRoductType

--- app/pages/productlist/page.tsx
+++ app/pages/productlist/page.tsx
@@ -1,26 +1,27 @@
-import { createClient } from '@/utils/supabase/server';
+import { createClient } from '@/utils/supabase/server'
 
 export default async function Page() {
-  const supabase = await createClient();
-  const { data: Products } = await supabase.from('Products').select('id,Name,Description,Price,ProductType(TypeName)');
+  const supabase = await createClient()
+  const { data: products } = await supabase
+    .from('Products')
+    .select('id, Name, Description, Price, ProductType(TypeName)')
 
   return (
     <ul role="list" className="divide-y divide-gray-100">
-      {Products != null ? (
-        Products.map((product) => (
+      {products && products.length > 0 ? (
+        products.map((product) => (
           <li key={product.id} className="flex justify-between gap-x-6 py-5">
             <div className="flex min-w-0 gap-x-4">
               <div className="min-w-0 flex-auto">
-                <p className="text-sm/6 font-semibold text-gray-900">{product.Name}</p>
-                <p className="mt-1 truncate text-xs/5 text-gray-500">{product.Description}</p>
-                <p className="mt-1 truncate text-xs/5 text-gray-500">{JSON.stringify(product.ProductType)}</p>
-                {product.ProductType && product.ProductType.TypeName && (
-                  <p className="mt-1 truncate text-xs/5 text-gray-500">{product.ProductType.TypeName}</p>
+                <p className="text-sm font-semibold leading-6 text-gray-900">{product.Name}</p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{product.Description}</p>
+                {product.ProductType && (
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{product.ProductType.TypeName}</p>
                 )}
               </div>
             </div>
             <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-              <p className="text-sm/6 text-gray-900">{product.Price}</p>
+              <p className="text-sm leading-6 text-gray-900">${product.Price.toFixed(2)}</p>
             </div>
           </li>
         ))
@@ -28,12 +29,5 @@ export default async function Page() {
         <li>No products found</li>
       )}
     </ul>
-  );
+  )
 }
-
-
-
-
-
-
-
