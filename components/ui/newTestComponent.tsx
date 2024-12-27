a944545a8188b90ec4c0d882705687cc3b296a12
emanuelrita <emanuelrita@gmail.com>
2024-12-27 09:40:14 +0000

    New test component 3

--- components/ui/newTestComponent.tsx
+++ components/ui/newTestComponent.tsx
@@ -5,7 +5,7 @@ export default function NewTestComponent(){
 
     return (
         <div>
-            <h1>Hello World!!  - just to run Qodana 2!</h1>
+            <h1>Hello World!!  - just to run Qodana 3!</h1>
         </div>
     )
 }
